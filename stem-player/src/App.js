dc5f518b961addf658f8d526af1190deaf24ebb0
vishnu rangiah <vrrangiah@ucdavis.edu>
2022-05-04 20:01:04 -0700

    recording works

--- stem-player/src/App.js
+++ stem-player/src/App.js
@@ -10,18 +10,29 @@ function App() {
   // let audio2 = new Audio("/FutureWaitForYou.mp3")
 
   let audio = new Audio(song)
+  let audio_on = false
 
   const start = () => {
     audio.play()
+    audio_on = true
   }
 
   const stop = () => {
     audio.pause()
+    audio_on = false
   }
 
+
+
+  const onChangeHandler = (e) => {
+    if(audio_on){
+      audio.pause()
+      audio_on = false
+    }
+    setSong(e.target.value)
+  };
+
   
-
-
   return (
     < div >
       <button onClick={start}>Play</button>
@@ -29,7 +40,7 @@ function App() {
 
       <p>{song}</p>
 
-      <select onChange={(e) => setSong(e.target.value)}>
+      <select onChange={onChangeHandler}>
         <option value="/Future712.mp3">song1</option>
         <option value="/FutureWaitForYou.mp3">song2</option>
       </select>
